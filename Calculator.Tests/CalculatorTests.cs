ddc9fcae0ec520154505b5e8326551f3b4362a7a
Navneet Kaur <navkaurneet1@gmail.com>
2024-05-30 16:39:28 -0400

    Add Subtract and Divide functions and wrote some test cases.

--- Calculator.Tests/CalculatorTests.cs
+++ Calculator.Tests/CalculatorTests.cs
@@ -1,3 +1,4 @@
+
 namespace Calculator.Tests;
 
 public class CalculatorTests
@@ -5,22 +6,82 @@ public class CalculatorTests
     [Fact]
     public void TestAdd()
     {
-        Assert.Equal(6L, Add.Eval(1L, 5L));
+        Assert.Equal(12L, Add.Eval(8L, 4L));
+    }
+    [Fact]
+    public void TestSubtract()
+    {
+        Assert.Equal(10L, Subtract.Eval(25L, 15L));
     }
     [Fact]
     public void TestMult()
     {
-        Assert.Equal(6L, Multiply.Eval(2L, 3L));
+        Assert.Equal(8L, Multiply.Eval(4L, 2L));
+    }
+    [Fact]
+    public void TestDivide()
+    {
+        Assert.Equal(5L, Divide.Eval(25L, 5L));
+    }
+    [Fact]
+    public void TestAdd2()
+    {
+        Assert.Equal(7L, Add.Eval(2L, 5L));
+    }
+    [Fact]
+    public void TestSubtract2()
+    {
+        Assert.Equal(32L, Subtract.Eval(37L, 5L));
+    }
+    [Fact]
+    public void TestMult2()
+    {
+        Assert.Equal(60L, Multiply.Eval(6L, 10L));
+    }
+    [Fact]
+    public void TestDivide2()
+    {
+        Assert.Equal(4L, Divide.Eval(40L, 10L));
+    }
+    [Fact]
+    public void TestAdd3()
+    {
+        Assert.Equal(69L, Add.Eval(66L, 3L));
+    }
+    [Fact]
+    public void TestSubtract3()
+    {
+        Assert.Equal(2L, Subtract.Eval(8L, 6L));
+    }
+    [Fact]
+    public void TestMult3()
+    {
+        Assert.Equal(12L, Multiply.Eval(3L, 4L));
+    }
+    [Fact]
+    public void TestDivide3()
+    {
+        Assert.Equal(7L, Divide.Eval(35L, 5L));
     }
     [Fact]
     public void TestAddOperation()
     {
-        Assert.Equal(8, Evaluator.Eval("+", 6, 2));
+        Assert.Equal(11, Evaluator.Eval("+", 9, 2));
+    }
+    [Fact]
+    public void TestSubtractOperation()
+    {
+        Assert.Equal(3, Evaluator.Eval("-", 11, 8));
     }
     [Fact]
     public void TestMultiplyOperation()
     {
-        Assert.Equal(12, Evaluator.Eval("*", 6, 2));
+        Assert.Equal(81, Evaluator.Eval("*", 9, 9));
+    }
+    [Fact]
+    public void TestDivideOperation()
+    {
+        Assert.Equal(9, Evaluator.Eval("/", 81, 9));
     }
 
 }
